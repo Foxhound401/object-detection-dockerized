0ed1780dc0caf0b9fc9ae1e30b28072eade30505
phungphuc <phungthienphuc18@gmail.com>
2019-06-24 17:15:16 +0700

    migrating stream to Docker

--- utils/app_utils.py
+++ utils/app_utils.py
@@ -1,7 +1,15 @@
 # import the necessary packages
-from threading import Thread
+import struct
+import six 
+import collections
+import cv2 
 import datetime
-import cv2
+import subprocess as sp
+import json 
+import numpy
+import time
+from matplotlib import colors
+from threading import Thread
 
 class FPS:
     def __init__(self):
@@ -93,3 +101,77 @@ class WebcamVideoStream:
 
     def getFrameCount(self):
         return int(self.stream.get(cv2.CAP_PROP_FRAME_COUNT))
+
+
+class HLSVideoStream:
+	def __init__(self, src):
+		# initialize the video camera stream and read the first frame
+		# from the stream
+
+		# initialize the variable used to indicate if the thread should
+		# be stopped
+		self.stopped = False
+
+		FFMPEG_BIN = "ffmpeg"
+
+		metadata = {}
+
+		while "streams" not in metadata.keys():
+			
+			print('ERROR: Could not access stream. Trying again.')
+
+			info = sp.Popen(["ffprobe", 
+			"-v", "quiet",
+			"-print_format", "json",
+			"-show_format",
+			"-show_streams", src],
+			stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)
+			out, err = info.communicate(b"ffprobe -v quiet -print_format json -show_format -show_streams http://52.91.28.88:8080/hls/live.m3u8")
+
+			metadata = json.loads(out.decode('utf-8'))
+			time.sleep(5)
+
+
+		print('SUCCESS: Retrieved stream metadata.')
+
+		self.WIDTH = metadata["streams"][0]["width"]
+		self.HEIGHT = metadata["streams"][0]["height"]
+
+		self.pipe = sp.Popen([ FFMPEG_BIN, "-i", src,
+				 "-loglevel", "quiet", # no text output
+				 "-an",   # disable audio
+				 "-f", "image2pipe",
+				 "-pix_fmt", "bgr24",
+				 "-vcodec", "rawvideo", "-"],
+				 stdin = sp.PIPE, stdout = sp.PIPE)
+		print('WIDTH: ', self.WIDTH)
+
+		raw_image = self.pipe.stdout.read(self.WIDTH*self.HEIGHT*3) # read 432*240*3 bytes (= 1 frame)
+		self.frame =  numpy.fromstring(raw_image, dtype='uint8').reshape((self.HEIGHT,self.WIDTH,3))
+		self.grabbed = self.frame is not None
+
+
+	def start(self):
+		# start the thread to read frames from the video stream
+		Thread(target=self.update, args=()).start()
+		return self
+
+	def update(self):
+		# keep looping infinitely until the thread is stopped
+		# if the thread indicator variable is set, stop the thread
+
+		while True:
+			if self.stopped:
+				return
+
+			raw_image = self.pipe.stdout.read(self.WIDTH*self.HEIGHT*3) # read 432*240*3 bytes (= 1 frame)
+			self.frame =  numpy.fromstring(raw_image, dtype='uint8').reshape((self.HEIGHT,self.WIDTH,3))
+			self.grabbed = self.frame is not None
+
+	def read(self):
+		# return the frame most recently read
+		return self.frame
+
+	def stop(self):
+		# indicate that the thread should be stopped
+		self.stopped = True
